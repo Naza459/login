0c16798302fadadbab9cf876a64826a4fa6e6830
Gabriel Carvajal <gabrielcarvajal1409@gmail.com>
2023-10-09 16:54:12 -0400

    Modificar el cambio de contraseña

--- src/app/password/password.component/password.component.component.ts
+++ src/app/password/password.component/password.component.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-password.component',
@@ -8,13 +9,13 @@ import { Location } from '@angular/common';
 })
 export class PasswordComponentComponent {
   newPassword: string;
-  confirmpassword: string;
+  confirmPassword: string;
   showNewPassword: boolean;
   showConfirmPassword: boolean;
-  
-  constructor(private location: Location) {
+
+  constructor(private location: Location, private http: HttpClient) {
     this.newPassword = '';
-    this.confirmpassword = '';
+    this.confirmPassword = '';
     this.showNewPassword = false;
     this.showConfirmPassword = false;
   }
@@ -26,17 +27,42 @@ export class PasswordComponentComponent {
   toggleShowConfirmPassword() {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
+
   goBack() {
     this.location.back();
   }
+
   changeButtonColor(buttonId: string, color: string) {
-    var button = document.getElementById(buttonId);
+    const button = document.getElementById(buttonId);
     if (button) {
       button.style.backgroundColor = color;
     }
   }
-  login() {
-    console.log(this.newPassword);
-    console.log(this.confirmpassword);
+
+  changePassword() {
+    if (this.newPassword !== this.confirmPassword) {
+      console.log('Las contraseñas no coinciden');
+      return;
+    }
+
+    const userId = "gabriel1407"; // Reemplaza con el ID del usuario correspondiente
+
+    const endpoint = `http://localhost:8000/companies/change_password/?username=${userId}`;
+
+    const payload = {
+      password: this.newPassword
+    };
+
+    this.http.put(endpoint, payload)
+      .subscribe(
+        (response) => {
+          console.log('Contraseña cambiada exitosamente:', response);
+          // Realiza acciones adicionales después de cambiar la contraseña si es necesario
+        },
+        (error) => {
+          console.error('Error al cambiar la contraseña:', error);
+          // Maneja el error de acuerdo a tus necesidades
+        }
+      );
   }
 }
