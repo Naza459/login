ce6efb6dd23b34b2441e697714d31602b346c8a6
gabriel <gabrielcarvajal1409@gmail.com>
2023-10-10 22:00:49 -0400

    Modulo de login y cambio de contraseña exitoso

--- src/app/login/login.component/login.component.component.ts
+++ src/app/login/login.component/login.component.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+
+interface LoginResponse {
+  status: number;
+}
 
 @Component({
   selector: 'app-login.component',
@@ -9,26 +15,67 @@ import { HttpClient } from '@angular/common/http';
 export class LoginComponentComponent {
   username: string;
   password: string;
-  
-  constructor(private http: HttpClient) {
+  errorMessage: string;
+
+  constructor(private http: HttpClient, private router: Router) {
     this.username = '';
     this.password = '';
+    this.errorMessage = '';
   }
-  
+
+  forgotPassword() {
+    // Obtener el nombre de usuario del localStorage
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      // Construir el cuerpo de la solicitud POST
+      const body = { username: storedUsername };
+
+      // Realizar la solicitud POST al endpoint de cambio de contraseña
+      const endpoint = `http://localhost:8000/companies/change_password/`;
+
+      this.http.post(endpoint, body).subscribe(
+        (response) => {
+          console.log('Solicitud de cambio de contraseña enviada:', response);
+          // Realiza acciones adicionales si es necesario
+        },
+        (error) => {
+          console.error('Error al enviar la solicitud de cambio de contraseña:', error);
+          // Maneja el error de acuerdo a tus necesidades
+        }
+      );
+    }
+  }
+
   login() {
     console.log(this.username);
     console.log(this.password);
-    
+
     // Realizar la solicitud HTTP POST al servicio de login
-    this.http.post('http://localhost:8000/companies/login/', {
+    this.http.post<LoginResponse>('http://localhost:8000/companies/login/', {
       username: this.username,
       password: this.password
     }).subscribe(response => {
-      console.log('Login successful', response);
-      // Aquí puedes realizar acciones adicionales después de un inicio de sesión exitoso
-    }, error => {
+      console.log('Login response', response);
+      if (response.status === 200) {
+        console.log('Login successful');
+        
+        // Realizar redireccionamiento al componente "Gerente"
+        this.router.navigate(['/Gerente']);
+      } else {
+        console.log('Login error');
+        if (response.status === 400) {
+          this.errorMessage = 'El usuario no existe'; // Mensaje de error específico para el caso de usuario no encontrado
+        } else {
+          this.errorMessage = 'Credenciales inválidas'; // Mensaje de error genérico para otros errores
+        }
+      }
+    }, (error: HttpErrorResponse) => {
       console.error('Login error', error);
-      // Aquí puedes manejar el error en caso de un inicio de sesión fallido
+      if (error.status === 400) {
+        this.errorMessage = 'El usuario no existe'; // Mensaje de error específico para el caso de usuario no encontrado
+      } else {
+        this.errorMessage = 'Credenciales inválidas'; // Mensaje de error genérico para otros errores
+      }
     });
   }
 }
--- src/app/password/password.component/password.component.component.ts
+++ src/app/password/password.component/password.component.component.ts
@@ -12,12 +12,23 @@ export class PasswordComponentComponent {
   confirmPassword: string;
   showNewPassword: boolean;
   showConfirmPassword: boolean;
+  username: string; // Variable para almacenar el nombre de usuario
+  passwordChanged: boolean | undefined;
+  passwordMismatch: boolean | undefined;
 
   constructor(private location: Location, private http: HttpClient) {
     this.newPassword = '';
     this.confirmPassword = '';
     this.showNewPassword = false;
     this.showConfirmPassword = false;
+    this.username = ''; // Inicializar la variable del nombre de usuario
+
+    // Obtener el nombre de usuario del localStorage
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      this.username = storedUsername;
+      console.log(this.username);
+    }
   }
 
   toggleShowNewPassword() {
@@ -41,28 +52,32 @@ export class PasswordComponentComponent {
 
   changePassword() {
     if (this.newPassword !== this.confirmPassword) {
-      console.log('Las contraseñas no coinciden');
-      return;
-    }
 
-    const userId = "gabriel1407"; // Reemplaza con el ID del usuario correspondiente
+    console.log('Las contraseñas no coinciden');
+    this.passwordMismatch = true; // Actualiza la variable a true para mostrar el mensaje de error
+    return;
+  } else {
+    this.passwordMismatch = false; // Actualiza la variable a false para ocultar el mensaje de error si estaba visible
+  }
 
-    const endpoint = `http://localhost:8000/companies/change_password/?username=${userId}`;
+    const endpoint = `http://localhost:8000/companies/change_password/?username=${this.username}`;
 
     const payload = {
       password: this.newPassword
     };
 
-    this.http.put(endpoint, payload)
-      .subscribe(
-        (response) => {
-          console.log('Contraseña cambiada exitosamente:', response);
-          // Realiza acciones adicionales después de cambiar la contraseña si es necesario
-        },
-        (error) => {
-          console.error('Error al cambiar la contraseña:', error);
-          // Maneja el error de acuerdo a tus necesidades
-        }
-      );
-  }
-}
+  this.http.put(endpoint, payload)
+  .subscribe(
+    (response) => {
+      console.log('Contraseña cambiada exitosamente:', response);
+      this.passwordChanged = true; // Actualiza la variable a true para mostrar el mensaje de éxito
+      // Realiza acciones adicionales después de cambiar la contraseña si es necesario
+    },
+    (error) => {
+      console.error('Error al cambiar la contraseña:', error);
+      this.passwordChanged = false; // Actualiza la variable a false para no mostrar el mensaje de éxito
+      // Maneja el error de acuerdo a tus necesidades
+    }
+  )
+  }   
+};
